7139e83257be3368b287bc4a7485dbf2e3c8516a
Manale Ben meziane <manale.ben-meziane.auditeur@lecnam.net>
2021-12-08 09:43:37 +0100

    Exo1

---
+++ tp6-ManaleBen/src/net/lecnam/ussi2a/tp6/Division.java
@@ -0,0 +1,11 @@
+package net.lecnam.ussi2a.tp6;
+
+public class Division {
+
+    public Division(){
+    }
+
+    public double division1(int a, int b){
+        return a/b;
+    }
+}
--- tp6-ManaleBen/src/net/lecnam/ussi2a/tp6/Main.java
+++ tp6-ManaleBen/src/net/lecnam/ussi2a/tp6/Main.java
@@ -2,7 +2,12 @@ package net.lecnam.ussi2a.tp6;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void exercice1(int a, int b) {
+        Division d = new Division();
+        System.out.println(d.division1(a,b));
+    }
+
+    public static void main(String[] args){
+        exercice1(10,0);
     }
 }
