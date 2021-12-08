008b56f78b056119a980120d697d2ffc0eac4c9b
Manale Ben meziane <manale.ben-meziane.auditeur@lecnam.net>
2021-12-08 10:37:14 +0100

    Exo 3

--- tp6-ManaleBen/src/net/lecnam/ussi2a/tp6/Main.java
+++ tp6-ManaleBen/src/net/lecnam/ussi2a/tp6/Main.java
@@ -1,18 +1,35 @@
 package net.lecnam.ussi2a.tp6;
 
+import java.io.IOException;
+
 public class Main {
 
     public static void exercice1(int a, int b) {
         Division d = new Division();
         System.out.println(d.division1(a,b));
     }
+
     public static void exercice2(int a, int b) {
         Division d = new Division();
         System.out.println(d.division2(a,b));
     }
+
+
+    public static void exercice3(int a, int b) {
+        Division d = new Division();
+        try {
+            double res = d.division3(a,b);
+            System.out.println(res);
+        } catch (IOException e){
+            System.out.println("Division par 0");
+        }
+    }
+
+
     public static void main(String[] args){
         //exercice1(10,0);
-        exercice2(10,0);
+        //exercice2(10,0);
+        exercice3(10,0);
 
     }
 }
